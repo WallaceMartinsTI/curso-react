936ada467c83e67c36699f5775db33e3f4fe25a0
Wallace Martins <wallace159santos@hotmail.com>
2023-07-28 16:20:06 -0300

    finish context api

--- curso_react/9_CONTEXT/context/src/pages/About.js
+++ curso_react/9_CONTEXT/context/src/pages/About.js
@@ -1,11 +1,19 @@
-import { useContext } from "react";
-import { CounterContext } from "../context/CounterContext";
+//import { useContext } from "react";
+//import { CounterContext } from "../context/CounterContext";
+
+import { useCounterContext } from "../hooks/useCounterContext";
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
 
 const About = () => {
-  const { counter } = useContext(CounterContext);
+  //const { counter } = useContext(CounterContext);
+  const { counter } = useCounterContext();
+
+  // 5 - context mais complexo
+  const { color, dispatch } = useTitleColorContext();
+
   return (
     <div>
-      <h1>About</h1>
+      <h1 style={{ color }}>About</h1>
       <p>Valor do contador: {counter}</p>
     </div>
   );
