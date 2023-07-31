b789025c7a60d808c283683cce0a49d9c90c1f78
Wallace Martins <wallace159santos@hotmail.com>
2023-07-31 17:11:27 -0300

    start hooks class

---
+++ curso_react/11_HOOKS/reacthooks/src/components/HookUseState.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { useState } from "react";
+
+const HookUseState = () => {
+  // 1 - useState
+  let userName = "João";
+
+  const [name, setName] = useState("Matheus");
+
+  const changeNames = () => {
+    userName = "João Souza";
+    setName("Matheus Battisti");
+
+    console.log(userName);
+  };
+
+  console.log(name);
+  return (
+    <div>
+      {/* 1 - useState */}
+      <h2>useState</h2>
+
+      <p>Variável: {userName}</p>
+      <p>useState: {name}</p>
+      <button onClick={changeNames}>Mudar nomes!</button>
+    </div>
+  );
+};
+
+export default HookUseState;
